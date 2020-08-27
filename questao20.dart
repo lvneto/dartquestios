8994b395edb8a5bed23ef06f7e9bc5502ffb3b5a
lvneto <lauro.neto.1995@gmail.com>
2020-08-27 13:27:16 -0300

    teste

--- questao20.dart
+++ questao20.dart
@@ -4,24 +4,20 @@ import 'dart:math';
 import 'dart:io';
 
 main() {
-    
   //print('Digite um número: ');
   //String strN = stdin.readLineSync();
   //int n = int.parse(strN);
-  for (int n = 2; n<=50 ;n++){
-
-  
+  for (int n = 2; n <= 50; n++) {
     int quantidadeDivisores = 0;
-    for (int d=1; d<=n; d++) {
-      if(n % d ==0) {
+    for (int d = 1; d <= n; d++) {
+      if (n % d == 0) {
         quantidadeDivisores++;
       }
-      
     }
-    if(quantidadeDivisores == 2){
+    if (quantidadeDivisores == 2) {
       print('$n');
     } //else {
-      //print('$n É composto');
+    //print('$n É composto');
     //}
   }
-} ///
+} ////////////////// teste
