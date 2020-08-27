af7e51e6e8515cfcbccfb65bc0e3d22532393219
lvneto <lauro.neto.1995@gmail.com>
2020-08-27 13:24:12 -0300

    Exercícios atualizada

---
+++ questao20.dart
@@ -0,0 +1,27 @@
+//Descobrir numeros primos
+
+import 'dart:math';
+import 'dart:io';
+
+main() {
+    
+  //print('Digite um número: ');
+  //String strN = stdin.readLineSync();
+  //int n = int.parse(strN);
+  for (int n = 2; n<=50 ;n++){
+
+  
+    int quantidadeDivisores = 0;
+    for (int d=1; d<=n; d++) {
+      if(n % d ==0) {
+        quantidadeDivisores++;
+      }
+      
+    }
+    if(quantidadeDivisores == 2){
+      print('$n');
+    } //else {
+      //print('$n É composto');
+    //}
+  }
+}
