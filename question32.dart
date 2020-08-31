7c69596aabd93249dbbfbe29c8fd577f7a66188a
lvneto <lauro.neto.1995@gmail.com>
2020-08-31 10:36:59 -0300

    questionsupdate

--- question32.dart
+++ question32.dart
@@ -1,20 +1,23 @@
-//Questão proposta no Desafio
+import 'dart:io';
 
 main() {
-  bool isNumber(String item) {
-    return 'leal123'.split('').contains(item);
-  }
+  print('Insira uma string: ');
+  String strP = stdin.readLineSync();
+  String sonumeros = "";
+  String inversa = "";
 
-  List<String> numbers = [
-    'L',
-    'E',
-    'A',
-    'L',
-    '1',
-    '2',
-    '3',
-  ];
-  print(numbers);
-  numbers.removeWhere((item) => !isNumber(item));
-  print(numbers.reversed.toList());
+  for (var i = 0; i < strP.length; i++) {
+    // Lê a String da posição 0 até o tamanho total da String
+    if (strP.codeUnitAt(i) >= '0'.codeUnitAt(0) &&
+        strP.codeUnitAt(i) <= '9'.codeUnitAt(0))
+    //Se i na posição 0 for igual ou maior que 0 E i na posição 0 for menor ou igual a 9
+    {
+      sonumeros = sonumeros + strP[i];
+      //soma a variável vazia sonumeros com a string[i] de onde foram retirados os numeros
+      inversa = strP[i] + inversa;
+      //soma a variável vazia sonumeros com a string[i] de onde foram retirados os numeros na ordem inversa
+    }
+  }
+  print(sonumeros);
+  print(inversa);
 }
